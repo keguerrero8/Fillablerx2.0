335504fb6db51e39a121b6e9436e0fe969ca55ee
grff11 <122590223+grff11@users.noreply.github.com>
2023-03-07 00:20:40 -0500

    HeroSection mobile

--- client/src/Components/ContactSection/ContactSection.js
+++ client/src/Components/ContactSection/ContactSection.js
@@ -6,7 +6,7 @@ import { Box, Button, Typography } from '@mui/material'
 
 import './ContactSection.css';
 import '../../App.css';
-import '../ButtonAction/ButtonAction.css';
+import '../Button/Button.css';
 
 function ContactSection() {
     const form = useRef();
