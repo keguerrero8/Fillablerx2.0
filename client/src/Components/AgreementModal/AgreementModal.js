15d06f25830cf4c880522fbd0fac688e44391703
Kevin Guerrero <guerrk83z@gmail.com>
2023-03-14 23:58:15 -0400

    make modal responsive

--- client/src/Components/AgreementModal/AgreementModal.js
+++ client/src/Components/AgreementModal/AgreementModal.js
@@ -25,7 +25,7 @@ export default function AgreementModal( {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: "800px",
+        width: {xs: "95%", sm: "90%", md: "80%", lg: "900px"},
         height: "80%",
         bgcolor: 'background.paper',
         border: '2px solid #000',
