15e97cf4dfdc192aeab2653d27af5796a848e2b0
Kevin Guerrero <guerrk83z@gmail.com>
2023-04-25 00:45:48 -0400

    remove csp headers

--- app/settings/base.py
+++ app/settings/base.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "csp.middleware.CSPMiddleware",
+    # "csp.middleware.CSPMiddleware",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
