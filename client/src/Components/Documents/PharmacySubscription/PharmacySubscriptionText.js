ceaf729f64d2d43b7898e00634e33bd303ec2532
Kevin Guerrero <guerrk83z@gmail.com>
2023-04-26 19:27:51 -0400

    update sub agreement for larry to print

--- client/src/Components/Documents/PharmacySubscription/PharmacySubscriptionText.js
+++ client/src/Components/Documents/PharmacySubscription/PharmacySubscriptionText.js
@@ -13,7 +13,7 @@ function PharmacySubscriptionText({ isModal = false, pharmacy, enrollmentData })
     <>
         <div className='parties'>
             <h3>ENROLLED PHARMACY SUBSCRIPTION AGREEMENT</h3>
-            <p>THIS SUBSCRIPTION AGREEMENT (this "Agreement") is made as of {formattedDate} ("the Effective Date") between Fillable, 
+            <p>THIS SUBSCRIPTION AGREEMENT (this "Agreement") is made as of April 24th, 2023 ("the Effective Date") between Fillable, 
              LLC, d/b/a FillableRx, a/k/a Kindly Oblige With (KOW) (hereinafter referred to as "KOW") on the one hand, and you, an enrolled pharmacy (hereinafter referred to as "You", "Your", or "Enrolled Pharmacy", together with KOW, the "Parties", each being a "Party"), on the other.</p>
         </div>
         <div className='recitals'>
@@ -59,9 +59,9 @@ function PharmacySubscriptionText({ isModal = false, pharmacy, enrollmentData })
                 to between the Parties for good and valuable consideration. For a specified term beginning on the
                 Effective Date and ending on Your receipt of the Termination and Fee Schedule Notice (as
                 hereinafter defined) (the “Initial Testing Period”), KOW will permit access to and enrollment in
-                its Services at the agreed upon monthly rate of ($insert from enrollmentData) which shall be the Initial Rate. 
+                its Services at the agreed upon monthly rate of $25 which shall be the Initial Rate. 
                 The Initial Rate represents a promotional rate based on the Enrolled Pharmacy's network 
-                selection at the time of enrollmenta nd offered by KOW during KOW's Initial Testing Period. 
+                selection at the time of enrollment and offered by KOW during KOW's Initial Testing Period. 
                 The duration of the Initial Testing Period shall be fixed by KOW in its sole and absolute 
                 discretion. Upon termination of the Initial Testing Period, KOW shall serve You with a notice 
                 advising You of the following:</p>
