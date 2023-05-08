a7c34c33ea5794551f8eeef4a9e63b11ad056136
grff11 <122590223+grff11@users.noreply.github.com>
2023-05-07 21:34:44 -0400

    updated freq requests for w/o 5/1

--- client/src/Components/FrequentRequests/FrequentRequests.js
+++ client/src/Components/FrequentRequests/FrequentRequests.js
@@ -6,24 +6,24 @@ import './FrequentRequests.css';
 function FrequentRequests() {
     const dataColumn1 = [
         { req: 1, name: 'Aprepitant 125mg-80mg' },
-        { req: 2, name: 'Linzess 290mg' },
-        { req: 3, name: 'Albuterol 2.5mg/3mL' },
+        { req: 2, name: 'Albuterol 2.5mg/3mL' },
+        { req: 3, name: 'Granisetron 1mg' },
       ];
     const dataColumn2 = [
-        { req: 1, name: 'Granisetron 1mg' },
-        { req: 2, name: 'Sucralfate 1mg' },
-        { req: 3, name: 'Megestrol 400mg/10mL' },
+        { req: 1, name: 'Xarelto 15mg' },
+        { req: 2, name: 'Xarelto Starter Pack' },
+        { req: 3, name: 'Entecavir 0.5mg' },
       ];
     const dataColumn3 = [
-        { req: 1, name: 'Imatinib 400mg' },
+        { req: 1, name: 'Eliquis Starter Pack' },
         { req: 2, name: 'Ondansetron ODT 8mg' },
-        { req: 3, name: 'Abiraterone 500mg' },
+        { req: 3, name: 'Megestrol 40mg/mL' },
       ];
 
     return (
             <div className='recent-container'>
                 <h2>Most Frequent Requests:</h2>
-                <h3>{"Week of 4/17/23"}</h3>
+                <h3>{"Week of 5/1/23"}</h3>
                 <div className='request-groups'>
                     <ReqList data={dataColumn1}/>
                     <ReqList data={dataColumn2}/>
